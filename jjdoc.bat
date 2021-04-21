0d9607d3f45981de4722488ff1e2d8fa2e82debd
Francis ANDRE <francis.andre.kampbell@orange.fr>
2021-04-21 08:54:47 +0200

    Reorg to avoid publishing the it pom

---
+++ jjdoc.bat
@@ -0,0 +1,16 @@
+echo off
+setlocal
+
+set M2=%USERPROFILE%\.m2\repository
+set CORE_VERSION=8.0.1
+set CORE=%M2%\org\javacc\core\%CORE_VERSION%\core-%CORE_VERSION%.jar
+
+set VERSION=-SNAPSHOT
+set JAVA_NUMBER=8.0.1
+set JAVA=java-%JAVA_NUMBER%%VERSION%
+
+set TARGET=target\%JAVA%.jar
+set CP=%CORE%;%TARGET%
+
+java -cp %CP% jjdoc %*
+endlocal
---
+++ pom.xml.java
@@ -0,0 +1,22 @@
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+	<parent>
+		<groupId>org</groupId>
+		<artifactId>javacc</artifactId>
+		<version>8.0.5-SNAPSHOT</version>
+		<relativePath />
+	</parent>
+	<groupId>org.javacc.generator</groupId>
+	<artifactId>java</artifactId>
+	<version>8.0.1-SNAPSHOT</version>
+	<packaging>pom</packaging>
+	<name>JavaCC default Java code generator</name>
+	<description>JavaCC default Java code generator</description>
+
+
+	<modules>
+		<module>it</module>
+		<module>java</module>
+	</modules>
+
+</project>
